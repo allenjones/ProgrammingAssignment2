b361fcf306f44da7d07f8e61bd84f8d9cc43b2cc
Allen Jones <allen@allencathern.onmicrosoft.com>
2016-08-14 01:46:12 -0400

    Added time measurements.

--- test_routine.R
+++ test_routine.R
@@ -8,30 +8,47 @@
 ## 3) Verifies that if a different matrix is passed to the cacheSolve routine,
 ##    then a new inverse matrix is returned.
 ## 4) Times each call to cacheSolve so that runtimes can be compared.
+
 source("cachematrix.R")
+
+options(digits.secs=6)
 
 testInverse <- function() {
     for (dimension in 2:5) {
+        
+        print(paste("Running test with a dimension of", dimension))
+        
         vectorA <- runif(dimension, max = 100)
-        print(vectorA)
+        
         cache1 <- makeVector(vectorA)
-        print(cache1)
+        
+        beginTime <- Sys.time()
         value1 <- cachemean(cache1)
-        print(value1)
+        endTime <- Sys.time()
+        print(paste("Elapsed time:", endTime - beginTime, "seconds."))
+        
+        beginTime <- Sys.time()
         value2 <- cachemean(cache1)
-        print(value2)
+        endTime <- Sys.time()
+        print(paste("Elapsed time:", endTime - beginTime, "seconds."))
+        
         if (value1 != value2) {
             return(FALSE)
         }
+        
         vectorA <- runif(dimension, max = 100)
-        print(vectorA)
+
         cache1 <- makeVector(vectorA)
-        print(cache1)
+        
+        beginTime <- Sys.time()
         value3 <- cachemean(cache1)
-        print(value3)
+        endTime <- Sys.time()
+        print(paste("Elapsed time:", endTime - beginTime, "seconds."))
+        
         if (value1 == value3) {
             return(FALSE)
         }
+        print('')
     }
     return(TRUE)
 }
